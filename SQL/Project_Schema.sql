5836afa7175fd7cb1b64ad4c5011370da1e183c4
chrisadamsv <chrisadams@Chris-MacBook-Pro-16.local>
2024-11-12 22:41:33 -0500

    Editing SQL file with CHECK command

--- SQL/Project_Schema.sql
+++ SQL/Project_Schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE Contacts (
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
     email VARCHAR NOT NULL
+	CHECK (email LIKE '%@%')
 );
 
 -- Create a table to store the Categories data.
@@ -29,16 +30,20 @@ CREATE TABLE Campaigns (
     description VARCHAR,
     goal DECIMAL(10,2),
     pledged DECIMAL(10,2),
-    outcome VARCHAR  NOT NULL,
+    outcome VARCHAR NOT NULL
+	CHECK (outcome = 'failed' 
+		  	OR outcome = 'successful'
+		  	OR outcome = 'live'
+		  	OR outcome = 'canceled'),
     backers_count INT,
     country VARCHAR(2) NOT NULL,
     currency VARCHAR(3) NOT NULL,
     launched_date DATE,
     end_date DATE,
-    category_id VARCHAR NOT NULL,
+    category_id VARCHAR NOT NULL
 	CHECK (category_id LIKE 'cat%'),
 	FOREIGN KEY (category_id) REFERENCES Categories (category_id),
-    subcategory_id VARCHAR NOT NULL,
+    subcategory_id VARCHAR NOT NULL
 	CHECK (subcategory_id LIKE 'subcat%'),
 	FOREIGN KEY (subcategory_id) REFERENCES Subcategories (subcategory_id)
 );
